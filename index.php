2226f390e498e6da2e3438893a346e7d848189c8
epedder <elric@novitraq.com>
2014-11-20 13:31:42 -0500

    - Implement Twig test

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-echo phpinfo();
+//echo phpinfo();
+
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+    'debug' => true,
+    'strict_variables' => true,
+));
+$twig->addExtension(new Twig_Extension_Debug());
+echo $twig->render('home.twig', array());
 
 ?>
