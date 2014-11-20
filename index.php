7f12dfd9a95c370a828c377aa62e841ed198b6be
epedder <elric@novitraq.com>
2014-11-20 13:39:59 -0500

    - Include Twig Autoloader

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 //echo phpinfo();
 
+require_once('Twig/Autoloader.php');
+
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
 $twig = new Twig_Environment($loader, array(
     'cache' => false,
